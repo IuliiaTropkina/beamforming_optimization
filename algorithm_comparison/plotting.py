561808b6786585745df314d66e67e1e8c97e4cef
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-04-30 14:24:32 +0300

    calculation commit

--- algorithm_comparison/plotting.py
+++ algorithm_comparison/plotting.py
@@ -778,10 +778,11 @@ def plot_real_protocol():
     plt.figure(fig_name3)
 
     its = np.linspace(0,ITER_NUMBER_CIR-1,ITER_NUMBER_CIR)
+    print(f"length, {ITER_NUMBER_CIR}")
     plt.plot(its * duration_of_one_sample, oracle_dBm)
 
 
-    plt.ylabel('Power, dB',fontsize=14)
+    plt.ylabel('Power, dBm',fontsize=14)
     plt.xlabel("Time, sec",fontsize=14)
     # plt.yscale("log")
     # plt.ylim(0,10)
