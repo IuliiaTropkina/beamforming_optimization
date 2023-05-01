98b54eeb736418a2a7411ff76fe78697f94da084
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-05-01 14:38:29 +0300

    calculation commit

--- algorithm_comparison/plotting.py
+++ algorithm_comparison/plotting.py
@@ -903,7 +903,8 @@ def plot_real_protocol():
                             f"{PATH}/exloitation_iterations_bandit_{alg_name}_cont_type{con_type}_cont_param{cont_param}_arms{int(ARMS_NUMBER_CIR)}_{p}_num_cycle{number_of_cycles}_SSBperiod{SSB_p}_consSSB{NUMBER_OF_CONS_SSB}_seed{1}.pickle",
                             "rb"))
 
-                        diff = np.zeros(len(exloitation_iterations)-window_size+1)
+                        #diff = np.zeros(len(exloitation_iterations)-window_size+1)
+                        diff = np.zeros(len(exloitation_iterations))
                         number_of_seeds = 10
                         for seed_num in range(1,number_of_seeds+1):
                             exloitation_iterations = pickle.load(open(
