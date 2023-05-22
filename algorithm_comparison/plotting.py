0ba1b80755e7b068805f18a3e77f0378d740c6ac
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-05-22 14:53:52 +0300

    plotting

--- algorithm_comparison/plotting.py
+++ algorithm_comparison/plotting.py
@@ -873,7 +873,7 @@ def plot_real_protocol():
 
 
     start_it = 0
-    leng = 40000 #370000 - start_it
+    leng = 170000 #370000 - start_it
     chosen_beam_number_seq_search = pickle.load(open(
         f"{PATH}/chosen_beam_number_seq_search_arms{int(ARMS_NUMBER_CIR)}_SSBperiod2_consSSB{BURST_calib}.pickle",
         "rb"))
