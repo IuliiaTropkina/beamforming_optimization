77346baffb03400061ab959b56d94db068580a98
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-05-23 21:47:09 +0300

    plotting

--- algorithm_comparison/plotting.py
+++ algorithm_comparison/plotting.py
@@ -965,8 +965,9 @@ def plot_real_protocol():
                 for N_f in Numbers_of_frames_between_SSB:
                     for n_b in NUMBERs_OF_CONS_SSB:
                         print(f"{PATH}/chosen_arm_type{con_type}_context{cont_param}_{a}_{p}_{ARMS_NUMBER_CIR}_SSBperiod{N_f}_consSSB{n_b}.pickle")
+                        print("chosen_arm_typelocation_context0_EPS_greedy_0.8_162_SSBperiod1_consSSB4.pickle")
                         chosen_beam_number_bandit = pickle.load(open(
-                            f"{PATH}/chosen_arm_type{con_type}_context{cont_param}_{a}_{p}_{ARMS_NUMBER_CIR}_SSBperiod{N_f}_consSSB{n_b}.pickle",
+                            f"{PATH}/chosen_arm_typelocation_context0_EPS_greedy_0.8_162_SSBperiod1_consSSB4.pickle",
                             "rb"))
 
                         fig_name3 = f"chosen_arm_type{con_type}_context{cont_param}_{a}_{p}_{ARMS_NUMBER_CIR}_SSBperiod{N_f}_consSSB{n_b}.pickle"
