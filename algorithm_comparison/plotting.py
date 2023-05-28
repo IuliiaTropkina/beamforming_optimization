81d4673d72c62da7f0ac5f1841e7d3ca0bccf4f4
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-05-28 18:24:04 +0300

    added throughput calculation

--- algorithm_comparison/plotting.py
+++ algorithm_comparison/plotting.py
@@ -678,6 +678,7 @@ def plot_exploitation_test():
     plt.show()
 
 def calculate_act_throughput(reward, exp_iterations, BAND_COEF,BANDWIDTH, noize_dB):
+    print(f"len r {len(reward)}")
     inst_throughput = BANDWIDTH*np.log2(1 + np.array(reward)/(10**(noize_dB/10)))
     print(f"len {len(inst_throughput)}")
     inst_throughput_exploration = inst_throughput[exp_iterations]
