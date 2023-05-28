22559778ef2a1ebd4d010f6e6e886734a0447b4b
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-05-29 00:46:26 +0300

    added throughput calculation

--- algorithm_comparison/main.py
+++ algorithm_comparison/main.py
@@ -849,12 +849,12 @@ if __name__ == '__main__':
     #                    "THS"]
     cont_params = [len(np.array(icosphere_context.vertices)), LOCATION_GRID_STEP]
     folder_test = "real_protocol"
-    algorithm_names = ["UCB"] #"DQL","EPS_greedy"
+    algorithm_names = ["EPS_greedy"] #"DQL","EPS_greedy"
     # parameters = [[0.05, 0.1, 0.15],
     #               [10 ** (-7), 10 ** (-7) * 2, 10 ** (-7) / 2],
     #               [0.2, 0.5]]
     #parameters = [[0.01,0.02, 0.2, 0.5]]#UCB
-    parameters = [[0.01]]  # eps greedy 0.8
+    parameters = [[0.8]]  # eps greedy 0.8
 
 
     def calc(number_of_frames_between_SB_burst,number_of_SB_in_burst):
