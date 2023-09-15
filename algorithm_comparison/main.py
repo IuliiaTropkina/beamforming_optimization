0b8cd56ce8f8745eab562b5d7e007017d1410d37
Iulia Tropkina <iulia.tropkina@gmail.com>
2023-09-15 17:17:38 +0300

    plotting

--- algorithm_comparison/main.py
+++ algorithm_comparison/main.py
@@ -1158,7 +1158,7 @@ if __name__ == '__main__':
         RX_locations.append(info["RX_location"][0])
         TX_locations.append(info["TX_location"][0])
         if fr != 1:
-            a = find_angle_between_vectors((RX_locations[len(RX_locations)] - TX_locations[len(RX_locations)]), (RX_locations[len(RX_locations)-1] - TX_locations[len(RX_locations)-1]))
+            a = find_angle_between_vectors(RX_locations[len(RX_locations)] - TX_locations[len(RX_locations)], RX_locations[len(RX_locations)-1] - TX_locations[len(RX_locations)-1])
             angles_between_adjacent_slots.append(a*180/math.pi)
 
     fig_name3 = f"angle_between_adj_slots"
